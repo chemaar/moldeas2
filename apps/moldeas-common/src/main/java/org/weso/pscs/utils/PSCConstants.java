466a9d7c24fc146dc2abd59d594ead3189a3987f
Jose María Alvarez <chema.ar@gmail.com>
2013-06-04 09:41:02 +0300

    Updating URIs

--- apps/moldeas-common/src/main/java/org/weso/pscs/utils/PSCConstants.java
+++ apps/moldeas-common/src/main/java/org/weso/pscs/utils/PSCConstants.java
@@ -27,16 +27,18 @@
  */
 package org.weso.pscs.utils;
 
+import org.weso.moldeas.utils.URISchemeManager;
+
 
 public class PSCConstants {
 
+	private static final String ID_PREFIX = "resource";
+
 	public static final String HTTP_NUTS_PSI_ENAKTING_ORG_ID = "http://nuts.psi.enakting.org/id/";
 	//FIXME: Change # by /
-	public static final String HTTP_WWW_W3_ORG_2008_05_SKOS_XL = "http://www.w3.org/2008/05/skos-xl#";
-	public static final String HTTP_WWW_W3_ORG_2004_02_SKOS_CORE = "http://www.w3.org/2004/02/skos/core#";
-
 	
-	public static final String SKOS_prefLabel = HTTP_WWW_W3_ORG_2008_05_SKOS_XL+"prefLabel";
+	public static final String HTTP_WWW_W3_ORG_2004_02_SKOS_CORE = URISchemeManager.getURIPrefix("skos");
+	public static final String SKOS_prefLabel = HTTP_WWW_W3_ORG_2004_02_SKOS_CORE+"prefLabel";
 	public static final String SKOS_example = HTTP_WWW_W3_ORG_2004_02_SKOS_CORE+"example";
 	public static final String SKOS_Concept = HTTP_WWW_W3_ORG_2004_02_SKOS_CORE+"Concept";
 	public static final String SKOS_Broader = HTTP_WWW_W3_ORG_2004_02_SKOS_CORE+"broader";
@@ -46,27 +48,28 @@ public class PSCConstants {
 	public static final String SKOS_EXACT_MATCH= HTTP_WWW_W3_ORG_2004_02_SKOS_CORE+"exactMatch";
 
 
+	public static final String BASE_URI=URISchemeManager.getURIPrefix("moldeas");
 
-	public static final String HTTP_PURL_ORG_WESO_PSCS = "http://purl.org/weso/pscs/";
-	public static final String HTTP_PURL_ORG_WESO_PSCS_DEF_LEVEL = "http://purl.org/weso/pscs/ontology/level";
-	public static final String HTTP_PURL_ORG_WESO_PSCS_DEF_RELATED_MATCH = "http://purl.org/weso/pscs/ontology/relatedMatch";
-	public static final String HTTP_PURL_ORG_WESO_CPV_2008 = HTTP_PURL_ORG_WESO_PSCS+"cpv/2008/";
-	public static final String HTTP_PURL_ORG_WESO_CPV_DEF = HTTP_PURL_ORG_WESO_PSCS+"cpv/ontology/";
-	public static final String HTTP_PURL_ORG_WESO_CPV_2003 = HTTP_PURL_ORG_WESO_PSCS+"cpv/2003/";
-	public static final String CPV2003_codeIn = HTTP_PURL_ORG_WESO_CPV_DEF+"codeIn2003";
-	public static final String CPV_codeIn = HTTP_PURL_ORG_WESO_CPV_DEF+"codeIn";
+	public static final String HTTP_PURL_ORG_WESO_PSCS = URISchemeManager.getURIPrefix("pscs");
+	public static final String HTTP_PURL_ORG_WESO_PSCS_DEF_LEVEL = URISchemeManager.getURIPrefix("pscs-onto")+"level";
+	public static final String HTTP_PURL_ORG_WESO_PSCS_DEF_RELATED_MATCH = URISchemeManager.getURIPrefix("pscs-onto")+"relatedMatch";
+	public static final String HTTP_PURL_ORG_WESO_CPV_2008 = URISchemeManager.getURIPrefix("cpv-2008");
+	public static final String HTTP_PURL_ORG_WESO_CPV_DEF = URISchemeManager.getURIPrefix("cpv-onto");
+	public static final String HTTP_PURL_ORG_WESO_CPV_2003 =  URISchemeManager.getURIPrefix("cpv-2003");
+	public static final String CPV2003_codeIn = URISchemeManager.getURIPrefix("cpv-onto")+"code-in-2003";
+	public static final String CPV_codeIn = URISchemeManager.getURIPrefix("cpv-onto")+"code-in";
 	
-	public static final String HTTP_PURL_ORG_WESO_CPV_DEF_DIVISION = HTTP_PURL_ORG_WESO_PSCS+"cpv/ontology/Division";
-	public static final String HTTP_PURL_ORG_WESO_CPV_DEF_GROUP = HTTP_PURL_ORG_WESO_PSCS+"cpv/ontology/Group";
-	public static final String HTTP_PURL_ORG_WESO_CPV_DEF_CLASS = HTTP_PURL_ORG_WESO_PSCS+"cpv/ontology/Class";
-	public static final String HTTP_PURL_ORG_WESO_CPV_DEF_CATEGORY = HTTP_PURL_ORG_WESO_PSCS+"cpv/ontology/Category";
+	public static final String HTTP_PURL_ORG_WESO_CPV_DEF_DIVISION = URISchemeManager.getURIPrefix("cpv-onto")+"Division";
+	public static final String HTTP_PURL_ORG_WESO_CPV_DEF_GROUP = URISchemeManager.getURIPrefix("cpv-onto")+"Group";
+	public static final String HTTP_PURL_ORG_WESO_CPV_DEF_CLASS = URISchemeManager.getURIPrefix("cpv-onto")+"Class";
+	public static final String HTTP_PURL_ORG_WESO_CPV_DEF_CATEGORY = URISchemeManager.getURIPrefix("cpv-onto")+"Category";
 	
-	public static final String CPV_CONCEPT = HTTP_PURL_ORG_WESO_CPV_DEF+"cpvConcept";
+	public static final String CPV_CONCEPT = URISchemeManager.getURIPrefix("cpv-onto")+"cpvConcept";
 	
-	public static final String HTTP_PURL_ORG_WESO_PPN_DEF = "http://purl.org/weso/ppn/def";
-	public static final String HTTP_PURL_ORG_WESO_PPN = "http://purl.org/weso/ppn";
-	public static final String CPV2008_codeIn = PSCConstants.HTTP_PURL_ORG_WESO_CPV_DEF+"codeIn2008";	
-	public static final String NUTS_CODE = HTTP_PURL_ORG_WESO_PPN_DEF+"nutsCode";
+	public static final String HTTP_PURL_ORG_WESO_PPN_DEF = URISchemeManager.getURIPrefix("moldeas-onto");
+	public static final String HTTP_PURL_ORG_WESO_PPN = URISchemeManager.getURIPrefix("moldeas-ppn");
+	//public static final String CPV2008_codeIn = PSCConstants.HTTP_PURL_ORG_WESO_CPV_DEF+"cpv-code";	
+	public static final String NUTS_CODE = URISchemeManager.getURIPrefix("moldeas-onto")+"nuts-code";
 	
 	
 	public static String formatNUTSTO(String id){	
@@ -75,19 +78,18 @@ public class PSCConstants {
 	 
 	//2   f(Id)=Skos-Uri
 	public static final String formatId2003(String id){
-		return HTTP_PURL_ORG_WESO_CPV_2003 +"resource/"+ id;
+		return HTTP_PURL_ORG_WESO_CPV_2003 +ID_PREFIX +"/"+ id;
 	}
 	
 
 	//3   f(Id)=Skos-Uri
 	public static String formatId(String id){	
-		return HTTP_PURL_ORG_WESO_CPV_2008 +"resource/"+ id ;
+		return HTTP_PURL_ORG_WESO_CPV_2008 +ID_PREFIX +"/"+ id ;
 	}
 	
 
 	public static String formatURIId(String id, String date) {
-		//FIXME
-		return PSCConstants.HTTP_PURL_ORG_WESO_PPN+"/"+date+"/"+id;
+		return PSCConstants.HTTP_PURL_ORG_WESO_PPN+date+"/"+ID_PREFIX+"/"+id;
 	}
 
 }
