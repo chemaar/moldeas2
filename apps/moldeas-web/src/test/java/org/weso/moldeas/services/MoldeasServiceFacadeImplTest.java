5be751837a5afa65cf617a66be9190b5229601ed
Jose María Alvarez <chema.ar@gmail.com>
2013-06-05 11:49:34 +0300

    Moving resources

--- apps/moldeas-web/src/test/java/org/weso/moldeas/services/MoldeasServiceFacadeImplTest.java
+++ apps/moldeas-web/src/test/java/org/weso/moldeas/services/MoldeasServiceFacadeImplTest.java
@@ -4,6 +4,7 @@ import junit.framework.Assert;
 
 import org.junit.Test;
 import org.weso.moldeas.to.DurationTO;
+import org.weso.moldeas.to.NUTSTO;
 import org.weso.moldeas.to.PPNResultTO;
 import org.weso.moldeas.to.PSCTO;
 import org.weso.moldeas.to.RequestSearchTO;
@@ -34,12 +35,37 @@ public class MoldeasServiceFacadeImplTest {
 		requestSearch.setYears(years);
 		//return new PPNResultTO();
 		//request.setMaxResults(10000);
-		requestSearch.setMaxResults(1000);
-		
+		requestSearch.setMaxResults(1000);		
 		PPNResultTO result = impl.search(requestSearch);;
 		Assert.assertEquals(18, result.getScoredPnnsTO().size());
-
-
+	}
+	
+	
+	@Test
+	public void testSearch() {
+		MoldeasServiceFacadeImpl impl = new MoldeasServiceFacadeImpl();
+		RequestSearchTO requestSearch = new RequestSearchTO();
+		PSCTO pstTO = new PSCTO();
+		pstTO.setUri("http://localhost/pscs/cpv/2008/resource/98510000");
+		requestSearch.getPscCodes().add(pstTO);
+		requestSearch.getNutsCodes().add(new NUTSTO("http://localhost/moldeas/nuts/resource/DE"));
+		DurationTO duration = new DurationTO();
+		duration.setMin(Long.valueOf(1));
+		duration.setMax(Long.valueOf(3));
+		requestSearch.setDuration(duration);
+		TotalCostTO totalCost = new TotalCostTO();
+		totalCost.setMin(Long.valueOf(75));
+		totalCost.setMax(Long.valueOf(300));
+		requestSearch.setTotalCost(totalCost );
+		YearsTO years = new YearsTO();
+		years.setMin(Long.valueOf(2009));
+		years.setMax(Long.valueOf(2010));
+		requestSearch.setYears(years);
+		//return new PPNResultTO();
+		//request.setMaxResults(10000);
+		requestSearch.setMaxResults(1000);		
+		PPNResultTO result = impl.enhancedSearch(requestSearch);;
+		Assert.assertEquals(16, result.getScoredPnnsTO().size());
 	}
 
 }
